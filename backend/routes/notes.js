3c59911f7c62fbcd4a7c0d3f108dd5a2018fb5fd
Rakesh60 <rakeshhupta5353@gmail.com>
2024-01-16 17:17:11 +0530

    all notes fetch

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -13,8 +13,32 @@ router.get('/fetchallnotes',fetchUser,async (req,res)=>{
         res.status(500).send("Internal Server Error");
     }
 
+})
+//* Route2: Add new Notes using Post
+router.post('/addnote',fetchUser,async(req,res)=>{
+    try {
+        // data coming from (frontend)
+        const {title,description,tag}=req.body;
+        // VAlidation 
+        if(!title || !description || !tag){
+            return res.status(400).json({error:"All Fields Are Required"});
+        }
 
+        const notes =await Notes({
+            title,
+            description,
+            tag,
+            user:req.userId
+        })
 
+        //Saving notes
+        const saveNote=await notes.save();
+        res.json(saveNote);
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).send("Internal Server Error");
+    }
 })
 
 
