012f8b4c8b7554ff4d7172df4cfadf39102c3c10
Rakesh60 <rakeshhupta5353@gmail.com>
2024-01-18 11:08:44 +0530

    pages created

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Sidebar from "../sidebar/Sidebar"
+import TopNavbar from "../topNavbar/TopNavbar"
+
+function Layout({children}) {
+  return (
+    <div>
+      {/* mobile Navbar hidden on desktop */}
+      <div className="lg:hidden">
+        <TopNavbar/>
+      </div>
+
+      <div className="max-h-screen flex">
+
+        {/* sidebar hidden on mobile */}
+        <nav className='w-72 flex-none ... hidden md:block'>
+            <Sidebar/>
+        </nav>
+
+        {/* main content */}
+        <main className="flex-1 min-w-0 overflow-auto...">
+            <div className="flex justify-center">
+                {children}
+            </div>
+        </main>
+      </div>
+
+      
+    </div>
+  )
+}
+
+export default Layout
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Layout from "../../components/layout/Layout";
+import NotesCard from "../../components/notecard/NoteCard";
 
 function Home() {
-  return <div>Home</div>;
+  return <div>
+
+    <Layout>
+    <NotesCard></NotesCard>
+    </Layout>
+  </div>;
 }
 
 export default Home;
