5398e3699c9f3903c6da967da8ff03e65dc6351a
Rakesh60 <rakeshgupta5353@gmail.com>
2024-01-18 23:56:30 +0530

    the signup is working properly

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,21 @@
 import connectToMongo from "./database/db.js";
-import express from 'express';
-import auth from './routes/auth.js'
-import notes from './routes/notes.js'
+import express from "express";
+import auth from "./routes/auth.js";
+import notes from "./routes/notes.js";
+import cors from "cors";
 
 connectToMongo();
-const app=express();
-const port=4000;
+const app = express();
+const port = 4000;
 
 // middle ware
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 // Available routes
-app.use('/api/auth',auth);
-app.use('/api/notes',notes)
+app.use("/api/auth", auth);
+app.use("/api/notes", notes);
 
-
-app.listen(port,()=>{
-    console.log(`App listening at port http://localhost:${port}`)
-})
+app.listen(port, () => {
+  console.log(`App listening at port http://localhost:${port}`);
+});
--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ function App() {
           <Route path="/profile" element={<Profile />} />
           <Route path="/*" element={<NoPage />} />
         </Routes>
+        <Toaster /> //Toaster
       </Router>
     </MyState>
   );
